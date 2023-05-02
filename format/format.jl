c339053f1d57fdee96e3c6d54530f91f9ac6c7ac
raphasampaio <rsampaio@psr-inc.com>
2023-05-01 21:56:46 -0300

    [skip ci] minor changes

--- format/format.jl
+++ format/format.jl
@@ -4,5 +4,5 @@ Pkg.instantiate()
 using JuliaFormatter
 
 for folder in ["docs", "format", "profiling", "revise", "src", "test"]
-    println("Formatted " * folder * ": " * format(joinpath(dirname(@__DIR__), folder)))
+    format(joinpath(dirname(@__DIR__), folder))
 end
