c427b31a5ffff2e526f49b04affe34316db2aac4
raphasampaio <rsampaio@psr-inc.com>
2023-05-06 21:32:12 -0300

    Project changes

--- src/ensemble/kmeans.jl
+++ src/ensemble/kmeans.jl
@@ -1,5 +1,5 @@
 function fit(algorithm::Kmeans, data::AbstractMatrix{<:Real}, result_gmm::GMMResult)
-    result_kmeans = convert(Kmeans, result_gmm)
+    result_kmeans = convert(KmeansResult, result_gmm)
     fit!(algorithm, data, result_kmeans)
     return result_kmeans
 end
--- src/results/equal.jl
+++ src/results/equal.jl
@@ -2,9 +2,9 @@ function Base.:(==)(a::KmeansResult, b::KmeansResult)
     return a.k == b.k && a.assignments == b.assignments && a.clusters == b.clusters
 end
 
-function Base.:(==)(a::KmedoidsResult, b::KmedoidsResult)
-    return a.k == b.k && a.assignments == b.assignments && a.clusters == b.clusters
-end
+# function Base.:(==)(a::KmedoidsResult, b::KmedoidsResult)
+#     return a.k == b.k && a.assignments == b.assignments && a.clusters == b.clusters
+# end
 
 function Base.:(==)(a::GMMResult, b::GMMResult)
     return a.k == b.k && a.assignments == b.assignments && a.weights == b.weights && a.clusters == b.clusters && a.covariances == b.covariances
