b158647a37936fe285457f497d265d579eba35c9
raphasampaio <rsampaio@psr-inc.com>
2023-05-06 22:02:13 -0300

    [skip ci] update docs

--- docs.bat
+++
@@ -1,2 +0,0 @@
-%JULIA_185% --project=docs -e "using Pkg; Pkg.develop(PackageSpec(path=pwd())); Pkg.instantiate()"
-%JULIA_185% --project=docs docs/make.jl
---
+++ docs/docs.bat
@@ -0,0 +1,2 @@
+%JULIA_185% --project -e "using Pkg; Pkg.develop(PackageSpec(path=dirname(pwd()))); Pkg.instantiate()"
+%JULIA_185% --project make.jl
