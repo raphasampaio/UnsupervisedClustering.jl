d8df3b3070dc7f4ddded75462c174ddb734d841e
raphasampaio <rsampaio@psr-inc.com>
2023-05-07 01:53:32 -0300

    [skip ci] update docs

--- src/metaheuristic/multistart.jl
+++ src/metaheuristic/multistart.jl
@@ -5,7 +5,12 @@
         max_iterations::Integer = 200
     )
 
-TODO: Documentation
+The MultiStart approach repeatedly applies a clustering algorithm to generate multiple solutions with different initial points and selects the best solution.
+
+# Fields
+- `local_search`: the clustering algorithm applied to improve the solution in each meta-heuristics iteration.
+- `verbose`: controls whether the algorithm should display additional information during execution.
+- `max_iterations`: represents the maximum number of iterations the algorithm will perform before stopping.
 """
 Base.@kwdef struct MultiStart <: ClusteringAlgorithm
     local_search::ClusteringAlgorithm
--- src/metaheuristic/randomswap.jl
+++ src/metaheuristic/randomswap.jl
@@ -6,7 +6,18 @@
         max_iterations_without_improvement::Integer = 150
     )
 
-TODO: Documentation
+The RandomSwap is a meta-heuristic approach used for clustering problems. It follows an iterative process that combines local optimization with perturbation to explore the search space effectively. A local optimization algorithm is applied at each iteration to converge toward a local optimum. Then, a perturbation operator generates a new starting point and continues the search.
+
+# Fields
+- `local_search`: the clustering algorithm applied to improve the solution in each meta-heuristics iteration.
+- `verbose`: controls whether the algorithm should display additional information during execution.
+- `max_iterations`: represents the maximum number of iterations the algorithm will perform before stopping.
+- `max_iterations_without_improvement`: represents the maximum number of iterations allowed without improving the best solution.
+
+# References
+* Fränti, Pasi.
+  Efficiency of random swap clustering.
+  Journal of big data 5.1, 2018
 """
 Base.@kwdef struct RandomSwap <: ClusteringAlgorithm
     local_search::ClusteringAlgorithm
