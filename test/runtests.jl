24aed3548d40d8ad06dd94815674d90d04370600
raphasampaio <rsampaio@psr-inc.com>
2023-09-04 21:59:31 -0300

    Updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -179,6 +179,23 @@ function test_all()
             @test result.converged == true
         end
 
+        @testset "ksegmentation" begin
+            result = concatenate(
+                UnsupervisedClustering.KsegmentationResult([1, 2], [1.0 2.0; 1.0 2.0; 1.0 2.0], 1.0, [0.5, 0.5], 1, 1.0, true),
+                UnsupervisedClustering.KsegmentationResult([1, 2], [1.0 2.0; 1.0 2.0; 1.0 2.0], 2.0, [1.0, 1.0], 2, 2.0, true),
+                UnsupervisedClustering.KsegmentationResult([1, 2, 2], [1.0 2.0; 1.0 2.0; 1.0 2.0], 3.0, [1.5, 1.5], 3, 3.0, true),
+            )
+
+            @test result.k == 6
+            @test result.assignments == [1, 2, 3, 4, 5, 6, 6]
+            @test result.clusters ≈ [1.0 2.0 1.0 2.0 1.0 2.0; 1.0 2.0 1.0 2.0 1.0 2.0; 1.0 2.0 1.0 2.0 1.0 2.0]
+            @test result.objective ≈ 6.0
+            @test result.objective_per_cluster ≈ [0.5, 0.5, 1.0, 1.0, 1.5, 1.5]
+            @test result.iterations == 6
+            @test result.elapsed ≈ 6.0
+            @test result.converged == true
+        end
+
         @testset "kmedoids" begin
             result = concatenate(
                 UnsupervisedClustering.KmedoidsResult([1, 2], [1, 2], 1.0, [0.5, 0.5], 1, 1.0, true),
