88cfcaceabe0b39aa3ae3d1c266b3a6c59011e17
raphasampaio <rsampaio@psr-inc.com>
2023-10-23 11:29:38 -0300

    Add stochastic_matrix(...)

---
+++ src/markov.jl
@@ -0,0 +1,18 @@
+function stochastic_matrix(k::Integer, from::AbstractVector{<:Integer}, to::AbstractVector{<:Integer})
+    @assert length(from) == length(to)
+
+    matrix = zeros(Float64, k, k)
+    for (i, j) in zip(from, to)
+        matrix[i, j] += 1.0
+    end
+    matrix = matrix ./ sum(matrix, dims = 1)
+
+    @assert !any(isnan, matrix)
+
+    return matrix
+end
+
+function stochastic_matrix(from::Result, to::Result)
+    @assert from.k == to.k
+    return stochastic_matrix(from.k, from.assignments, to.assignments)
+end
