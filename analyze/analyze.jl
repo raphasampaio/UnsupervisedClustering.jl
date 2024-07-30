94f7f37805f4a1450580742dc011a0786b8427af
raphasampaio <raphaelaraujosampaio@gmail.com>
2024-07-30 02:14:33 -0300

    Updated project

--- analyze/analyze.jl
+++ analyze/analyze.jl
@@ -8,4 +8,9 @@ Pkg.instantiate()
 
 using UnsupervisedClustering
 
-@show report_package(UnsupervisedClustering)
+package_path = dirname(@__DIR__)
+module_path = joinpath(package_path, "src/UnsupervisedClustering.jl")
+
+@show report_file(module_path; analyze_from_definitions = true)
+
+# @show report_package(UnsupervisedClustering)
