cc43e94a52f00f10e882d0bd37e7daefad44d9f9
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-08-02 17:33:06 -0300

    Update

--- docs/docs.bat
+++ docs/docs.bat
@@ -2,5 +2,5 @@
 
 SET BASEPATH=%~dp0
 
-CALL julia +1.11 --project -e "using Pkg; Pkg.develop(PackageSpec(path=dirname(pwd()))); Pkg.instantiate()"
-CALL julia +1.11 --project %BASEPATH%\make.jl
+CALL julia +1.11 --project=%BASEPATH% -e "using Pkg; Pkg.develop(PackageSpec(path=dirname(pwd()))); Pkg.instantiate()"
+CALL julia +1.11 --project=%BASEPATH% %BASEPATH%\make.jl
