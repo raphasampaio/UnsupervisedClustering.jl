51c0742288b7cbb6e9bccd53bb778ffb3b435913
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-09-27 13:35:56 -0300

    Update

--- src/distances.jl
+++ src/distances.jl
@@ -1,4 +1,4 @@
-function pairwise_distances!(
+function compute_distances!(
     metric::SemiMetric;
     distances::AbstractMatrix{<:Real},
     clusters::Matrix{<:Real},
@@ -8,29 +8,25 @@ function pairwise_distances!(
     return nothing
 end
 
-function pairwise_distances!(
+function compute_distances(
+    metric::SemiMetric;
+    clusters::Matrix{<:Real},
+    data::AbstractMatrix{<:Real},
+)
+    return pairwise(metric, clusters, data', dims = 2)
+end
+
+function compute_distances!(
     kmeans::AbstractKmeans;
     distances::AbstractMatrix{<:Real},
     result::KmeansResult,
     data::AbstractMatrix{<:Real},
 )
-    pairwise_distances!(
+    compute_distances!(
         kmeans.metric;
         distances = distances,
         clusters = result.clusters,
         data = data,
     )
     return nothing
-end
-
-function pairwise_distances(
-    kmeans::AbstractKmeans;
-    result::KmeansResult,
-    data::AbstractMatrix{<:Real},
-)
-    n, _ = size(data)
-    k = result.k
-    distances = zeros(k, n)
-    pairwise_distances!(kmeans; result, distances, data)
-    return distances
 end
